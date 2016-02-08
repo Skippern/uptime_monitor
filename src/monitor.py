ccbb161e6602266eb6c689a3b055ab37e6cc076f
Aun Johnsen <lists@gimnechiske.org>
2016-02-08 11:37:08 -0200

    including calculations of up/down-time

--- src/monitor.py
+++ src/monitor.py
@@ -3,35 +3,49 @@
 #
 # Python solution
 #
-
 import os
 import logging
 import time
-#import pyping
+import datetime
+
+DEBUG = 0
+interval = 15
 
 hostname = "google.com" # example
 response = os.system("ping -c 1 " + hostname + " > /dev/null 2>&1")
 status = response
-logging.basicConfig(filename='/Users/skippern/log/linklogger.log',level=logging.DEBUG,format='%(asctime)s %(message)s',datefmt='%Y/%m/%d %H:%M:%S:')
-logging.debug('monitor.py started')
-
-if response == 0:
-    logging.info('Network is up!')
+change = datetime.datetime.now().replace(microsecond=0)
+if DEBUG == 1:
+    logging.basicConfig(filename='/Users/skippern/log/linklogger.log',level=logging.DEBUG,format='%(asctime)s %(message)s',datefmt='%Y/%m/%d %H:%M:%S:')
 else:
-    logging.info('Network is down!')
+    logging.basicConfig(filename='/Users/skippern/log/linklogger.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%Y/%m/%d %H:%M:%S:')
+
+logging.debug('monitor.py started in DEBUG mode')
+if response == 0:
+    logging.info('monitory.py started. Network is up!')
+else:
+    logging.info('monitory.py started. Network is down!')
     response = 1
 
 keepalive = 1
 while (keepalive == 1):
-    time.sleep(45)
+    time.sleep(interval)
     response = os.system("ping -c 1 " + hostname + " > /dev/null 2>&1")
     if response == 2:
         response = 1
-    logging.debug('Response is %s', response)
+    logging.debug('Response is %s, status is %s', response, status)
     if response != status:
         if response == 0:
             ## Link just came back
-            logging.info('Link returned')
+            now = datetime.datetime.now().replace(microsecond=0)
+            diff = now - change
+            change = now
+            logging.info('Link returned, downtime: %s', diff)
+            status = response
         else:
             ## Link just fell
-            logging.warning('Link just died')
+            now = datetime.datetime.now().replace(microsecond=0)
+            diff = now - change
+            change = now
+            logging.warning('Link just died, uptime: %s', diff)
+            status = response
